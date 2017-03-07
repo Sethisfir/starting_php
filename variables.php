7647df2cc09158e6e241e217c1a2faabfd7ef52b
Sethisfir <micka_8@hotmail.fr>
2017-03-07 14:09:21 +0100

    exercice 4

--- variables.php
+++ variables.php
@@ -1,16 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="style/css/style.css">
+</head>
+<body>
 <?php
 	//Exercice 1
 	$nom='DURAND';
-	echo $nom;
+	echo "<p>".$nom."</p>";
 	//Exercice 2
 	$prenom='Mickaël';
 	$age=28;
-	echo $nom, $prenom, $age;
+	echo "<p>".$nom."</p>","<p>".$prenom."</p>","<p>".$age."</p>";
 	//Exercice 3
 	$km=1;
-	echo $km;
+	echo "<p>".$km."</p>";
 	$km=3;
-	echo $km;
+	echo "<p>".$km."</p>";
 	$km=125;
-	echo $km;
+	echo "<p>".$km."</p>";
+	//Exercice 4
+	$string="coucou";
+	$int=42;
+	$float=3.5;
+	$boolean=true;
+	echo "<p>".$string."</p>","<p>".$int."</p>","<p>".$float."</p>","<p>".$boolean."</p>"
 ?>
+	
+</body>
+</html>
