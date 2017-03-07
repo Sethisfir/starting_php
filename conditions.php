45cfdc48bf573e42fb3925ac123692be99ad0af4
Sethisfir <micka_8@hotmail.fr>
2017-03-07 16:33:47 +0100

    Exercice 2.2

--- conditions.php
+++ conditions.php
@@ -4,16 +4,29 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>conditions</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="style/css/index.css">
 </head>
 <body>
 	<div id="container">
 		<div class="exo">
 			<h2>Exercice 1</h2>
 			<?php
-				$age=(int)Null;
+				$age=21;
 				if ($age>18){
 					echo 'Vous êtes majeur !';
+				}else{
+					echo 'Vous êtes mineur !';
+				}
+			?>
+		</div>
+		<div class="exo">
+			<h2>Exercice 2</h2>
+			<?php
+				$iseasy=true;
+				if ($iseasy == true){
+					echo 'C\'est facile';
+				}else{
+					echo 'C\'est difficile';
 				}
 			?>
 		</div>
