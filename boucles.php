d65ff24e80e09dc928aa103f63032ddabe190b98
Sethisfir <micka_8@hotmail.fr>
2017-03-08 10:54:24 +0100

    exercice 3.8

--- boucles.php
+++ boucles.php
@@ -30,34 +30,58 @@
 			}			
 			?>
 		</div>
-		<div class="exo">
+		<div class="exo scroll">
 			<h2>Exercice 3</h2>
 			<?php
+			$maVariable=100;
+			$variable=42;
+			while($maVariable>=10){
+				$total=$maVariable*$variable;
+				echo $total.'/ ';
+				$maVariable--;
+			}
 			?>
 		</div>
 		<div class="exo">
 			<h2>Exercice 4</h2>
 			<?php
+			$maVariable=1;
+			while($maVariable<10){
+				echo $maVariable.'/ ';
+				$maVariable=$maVariable+($maVariable/2);
+			}
 			?>
 		</div>
-		<div class="exo">
+		<div class="exo scroll">
 			<h2>Exercice 5</h2>
 			<?php
+			for ($i=1; $i<15 ; $i++){ 
+				echo 'On y arrive presque ! <br>';
+			}
 			?>
 		</div>
-		<div class="exo">
+		<div class="exo scroll">
 			<h2>Exercice 6</h2>
 			<?php
+			for ($i=20; $i>0; $i--){
+				echo 'C\'est presque bon ! <br>';
+			}
 			?>
 		</div>
 		<div class="exo">
 			<h2>Exercice 7</h2>
 			<?php
+			for ($i=1; $i<100 ; $i=$i+15){ 
+				echo 'On tien le bamboo ! <br>';
+			}
 			?>
 		</div>
-		<div class="exo">
+		<div class="exo scroll">
 			<h2>Exercice 8</h2>
 			<?php
+			for ($i=200; $i>0 ; $i=$i+12){ 
+				echo 'Enfin !!! <br>';
+			}
 			?>
 		</div>
 	</div>
